543abb88f3813fce4e79244c55ce9a965d9388cc
A Torin <alberto.torin@gmail.com>
2017-11-19 21:39:26 +0100

    stile: Migliora la struttura del codice
    Le funzioni helper dello script sono posizionate all'inizio.

--- normattiva2rst.py
+++ normattiva2rst.py
@@ -1,6 +1,64 @@
 import re
 import sys
 
+# === Helper functions
+
+def removeDash(ln):
+    if re.match(r'----', ln):
+        return ""
+    else:
+        return ln
+
+
+def subsAccent(l):
+    '''
+    Sostituisce apostrofo con l'accento in parole che finiscono per vocale. 
+    Es: liberta' -> libertà
+    Po', po', è vengono ripristinati nella versione corretta
+    '''
+    l = l.replace("a'", "à").replace("e'", "é").replace("i'", "ì")
+    l = l.replace("o'", "ò").replace("u'", "ù").replace("E'", "È")
+    l = l.replace("pò", "po'").replace("Pò", "Po'").replace(" é ", " è ").replace("e\\\'", "è")
+    if l[-2:]==" é":
+        l = l[:-1] + "è"
+    if l[0:2]=="é ":
+        l = "è" + l[1:]
+        
+    return l
+
+
+def checkAdd(ln, title, flag):
+    if ln=="":
+        if title.startswith("Art."):
+            title = "**" + title + "**"
+        print(title)
+        flag = 0
+    else:
+        ln = ln[0] + ln[1:].lower()
+        title = title + " " + ln
+    return title, flag
+
+
+def checkLetter(ln):
+    if ln!="":
+        for lat in latin:
+            if (re.match(r'^[a-z]'+lat+'\) ', ln)): return True
+            elif (re.match(r'^[a-z][a-z]'+lat+'\) ', ln)): return True
+        else: return False
+    else: return False
+
+
+
+def checkNumber(ln):
+    if ln!="":
+        for lat in latin:
+            if re.match(r'^[0-9]'+lat+'\. ', ln): return True
+        else: return False    
+    else: return False
+
+
+# === Main script
+
 filename = sys.argv[1]
 
 latin = ["",
@@ -21,35 +79,12 @@ with open(filename, 'r') as f:
     text = f.read().split('\n')
 
 ctxt = [line.strip().replace(" "*5, " ").replace(" "*4, " ").replace(" "*3, " ").replace(" "*2, " ") for line in text]
-
-def removeDash(ln):
-    if re.match(r'----', ln):
-        return ""
-    else:
-        return ln
-
 # replace "((...))" with nothing
 ctxt = [line.replace("((...))","") for line in ctxt]
 # replace "((" "))" with nothing
 ctxt = [line.replace("((","").replace("))","") for line in ctxt]
 # replace line with nothing
 ctxt = [removeDash(line) for line in ctxt]
-
-def subsAccent(l):
-    '''
-    Sostituisce apostrofo con l'accento in parole che finiscono per vocale. 
-    Es: liberta' -> libertà
-    Po', po', è vengono ripristinati nella versione corretta
-    '''
-    l = l.replace("a'", "à").replace("e'", "é").replace("i'", "ì")
-    l = l.replace("o'", "ò").replace("u'", "ù").replace("E'", "È")
-    l = l.replace("pò", "po'").replace("Pò", "Po'").replace(" é ", " è ").replace("e\\\'", "è")
-    if l[-2:]==" é":
-        l = l[:-1] + "è"
-    if l[0:2]=="é ":
-        l = "è" + l[1:]
-        
-    return l
 
 ctxt = [subsAccent(line) for line in ctxt]
 
@@ -60,39 +95,6 @@ for (line, ind) in zip(ctxt, range(len(ctxt))):
     if ((line[:4]=="Capo") | (line[:4]=="Sezi") | (line[:4]=="Art.")) & (ctxt[ind-1]!=""):
         new_ctxt.append("")
     new_ctxt.append(line)
-
-
-
-def checkAdd(ln, title, flag):
-    if ln=="":
-        if title.startswith("Art."):
-            title = "**" + title + "**"
-        print(title)
-        flag = 0
-    else:
-        ln = ln[0] + ln[1:].lower()
-        title = title + " " + ln
-    return title, flag
-
-
-
-def checkLetter(ln):
-    if ln!="":
-        for lat in latin:
-            if (re.match(r'^[a-z]'+lat+'\) ', ln)): return True
-            elif (re.match(r'^[a-z][a-z]'+lat+'\) ', ln)): return True
-        else: return False
-    else: return False
-
-
-
-def checkNumber(ln):
-    if ln!="":
-        for lat in latin:
-            if re.match(r'^[0-9]'+lat+'\. ', ln): return True
-        else: return False    
-    else: return False
-
 
 
 isArt = 0
